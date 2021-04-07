cb0949c6556b4fbe74c277bcff4713e7abf1aaa6
Cassio Lopes <cassio.lopes@dbasuporte.com>
2021-04-07 18:17:41 -0300

    fix: set correct model to ProdutoRepository

--- src/main/java/compasso/test/springboot/catalogoprodutos/config/repository/ProdutoRepository.java
+++ src/main/java/compasso/test/springboot/catalogoprodutos/config/repository/ProdutoRepository.java
@@ -1,9 +1,10 @@
 package compasso.test.springboot.catalogoprodutos.config.repository;
 
+import compasso.test.springboot.catalogoprodutos.config.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<ProdutoRepository, Long> {
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
 }
