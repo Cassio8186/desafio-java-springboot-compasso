9d16f3e55e18b8118670da92cd728eeae2b3642b
Cassio Lopes <cassio.lopes@dbasuporte.com>
2021-04-07 15:06:56 -0300

    feat: add swagger docs

---
+++ src/main/java/compasso/test/springboot/catalogoprodutos/config/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package compasso.test.springboot.catalogoprodutos.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+	private static final String BASE_PACKAGE = "compasso.springboot.test.catalogoprodutos";
+	private final String appVersion;
+
+	public SwaggerConfig(@Value("${app.version}") String appVersion) {
+		this.appVersion = appVersion;
+	}
+
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
+				.build()
+				.apiInfo(this.metaData());
+
+	}
+
+	private ApiInfo metaData() {
+		return new ApiInfoBuilder()
+				.title("Catálogo de produtos")
+				.description("Neste microserviço deve ser possível criar, alterar, visualizar e excluir um determinado produto, além de visualizar a lista de produtos atuais disponíveis. Também deve ser possível realizar a busca de produtos filtrando por name, description e price.")
+				.version(this.appVersion)
+				.build();
+	}
+
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
+}
