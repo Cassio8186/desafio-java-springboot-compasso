18059792d7315cad3e5fce838644c88e445d5c6b
Cassio Lopes <cassio.lopes@dbasuporte.com>
2021-04-07 15:04:43 -0300

    Initial commit

---
+++ src/main/java/compasso/test/springboot/catalogoprodutos/CatalogoprodutosApplication.java
@@ -0,0 +1,13 @@
+package compasso.test.springboot.catalogoprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoprodutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoprodutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/compasso/test/springboot/catalogoprodutos/CatalogoprodutosApplicationTests.java
@@ -0,0 +1,13 @@
+package compasso.test.springboot.catalogoprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoprodutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
