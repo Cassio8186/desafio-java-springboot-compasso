dfcde33ba8896a788e0937b32489d5c640e2ef72
Cassio Lopes <cassio.lopes@dbasuporte.com>
2021-04-08 17:27:33 -0300

    fix (ProductController): Change httpStatus to 201

--- src/main/java/compasso/test/springboot/catalogoprodutos/controller/ProductController.java
+++ src/main/java/compasso/test/springboot/catalogoprodutos/controller/ProductController.java
@@ -4,6 +4,7 @@ import compasso.test.springboot.catalogoprodutos.model.Product;
 import compasso.test.springboot.catalogoprodutos.model.dto.ProductDTO;
 import compasso.test.springboot.catalogoprodutos.model.dto.ProductSaveDTO;
 import compasso.test.springboot.catalogoprodutos.service.ProductService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,7 +27,7 @@ public class ProductController {
 		final Product savedProduct = this.productService.save(product);
 		final ProductDTO savedProductDTO = ProductDTO.fromEntity(savedProduct);
 
-		return ResponseEntity.ok(savedProductDTO);
+		return new ResponseEntity<>(savedProductDTO, HttpStatus.CREATED);
 	}
 
 	@PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
--- src/test/java/compasso/test/springboot/catalogoprodutos/controller/ProductControllerTest.java
+++ src/test/java/compasso/test/springboot/catalogoprodutos/controller/ProductControllerTest.java
@@ -9,8 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import java.math.BigDecimal;
 
@@ -46,7 +48,7 @@ class ProductControllerTest {
 				.contentType(MediaType.APPLICATION_JSON_VALUE)
 				.content(json))
 				.andDo(print())
-				.andExpect(status().isOk())
+				.andExpect(status().isCreated())
 				.andExpect(jsonPath("$.name").value(product.getName()))
 				.andExpect(jsonPath("$.id").isNumber())
 				.andExpect(jsonPath("$.description").value(product.getDescription()))
