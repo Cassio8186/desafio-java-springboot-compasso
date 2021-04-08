11d8e657dffa81e3a0e394351fe86f2bf4408138
Cassio Lopes <cassio.lopes@dbasuporte.com>
2021-04-08 18:19:50 -0300

    feat: add exception handling for validation

--- src/main/java/compasso/test/springboot/catalogoprodutos/exception/RestExceptionHandler.java
+++ src/main/java/compasso/test/springboot/catalogoprodutos/exception/RestExceptionHandler.java
@@ -4,16 +4,38 @@ import compasso.test.springboot.catalogoprodutos.model.dto.ErrorDTO;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		StringBuilder stringBuilder = new StringBuilder();
+		List<FieldError> allErrors = ex.getBindingResult().getFieldErrors();
+		for (int i = 0, allErrorsSize = allErrors.size(); i < allErrorsSize; i++) {
+			FieldError error = allErrors.get(i);
+			final String fieldErrorMessage = error.getField() + ": " + error.getDefaultMessage();
+			stringBuilder.append(fieldErrorMessage);
+			final boolean isNotLastIteration = i != allErrorsSize - 1;
+			if (isNotLastIteration) {
+				stringBuilder.append(", ");
+			} else {
+				stringBuilder.append(".");
+			}
+		}
+		final String message = stringBuilder.toString();
+		final ErrorDTO errorDTO = new ErrorDTO(BAD_REQUEST.value(), message);
+		return new ResponseEntity<>(errorDTO, new HttpHeaders(), BAD_REQUEST);
+	}
 
 	@ExceptionHandler(EntityNotFoundException.class)
 	public final ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex) {
