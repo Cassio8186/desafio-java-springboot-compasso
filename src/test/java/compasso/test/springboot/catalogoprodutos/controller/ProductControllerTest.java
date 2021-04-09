ccb0859727ae8f4546bfe70109ddcc6e1f2976d0
Cassio Lopes <cassio.lopes@dbasuporte.com>
2021-04-09 15:03:35 -0300

    tests(ProductController): set h2 as active profile

--- src/test/java/compasso/test/springboot/catalogoprodutos/controller/ProductControllerTest.java
+++ src/test/java/compasso/test/springboot/catalogoprodutos/controller/ProductControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
@@ -31,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("h2")
 class ProductControllerTest {
 
 	private final Gson gson = new Gson();
